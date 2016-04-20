01a97b1d9c543ce00f9efe2a885c1e2f5bc3f8c9
agent809 <mragent809@gmail.com>
2016-04-20 06:41:29 -0400

    Made config file able to take in any uninstall/install command

--- checkFiles.py
+++ checkFiles.py
@@ -1,3 +1,21 @@
+install_command = ""
+uninstall_command = ""
+
+def init_config():
+	global install_command
+	global uninstall_command
+	conf_file = open("pshutdown.cfg", "r")
+	for i in conf_file.readlines():
+		if "install_command=" in i:
+			install_command = (i[i.index('=') + 1:]).strip("\n")
+			
+		elif "remove_command=" in i:
+			uninstall_command = (i[i.index('=') + 1:]).strip("\n")
+	conf_file.close()
+	return 1
+
+		
+
 def cleanup_list(list_in):
 	for i in range(0, len(list_in)):
 		list_in[i] = list_in[i].strip("\n")
@@ -5,19 +23,23 @@ def cleanup_list(list_in):
 		
 
 def program_check(actionPrograms, installedPrograms, removedPrograms):
+	global install_command
+	global uninstall_command
 	revert = open("revert", 'w')
 	revert.write("#!/bin/bash\n")
 	for i in actionPrograms:
 		if i in installedPrograms:
-			revert.write("pacman -R \"" + i + "\"\n")
+			revert.write(uninstall_command + " \"" + i + "\"\n")
 		elif i in removedPrograms:
-			revert.write("pacman -S \"" + i + "\"\n")
+			revert.write(install_command + " \"" + i + "\"\n")
 		else:
 			print(installedPrograms, removedPrograms)
 			print("Oh crap, something went seriously wrong...")
 	revert.close()
 
 def main():
+	global install_command
+	global uninstall_command
 	startfile = open("STARTUP.txt", 'r')
 	shutdownfile = open("SHUTDOWN.txt", 'r')
 	outputfile = open("output.txt", 'w')
@@ -100,4 +122,10 @@ def main():
 
 		
 if __name__ == "__main__":
-	main()
+	try:
+		init_config()
+	except Exception as e:
+		print("An error ocurred while loading the config file... ")
+		print(e)
+	else:
+		main()
