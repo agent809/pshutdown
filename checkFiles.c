abfed83c6dcb4a320535af2a810e4e6567fe240d
agent809 <mragent809@gmail.com>
2016-04-25 18:33:35 -0400

    Added basis for version of code in C

---
+++ checkFiles.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+char [] getInCommand(){
+	char file[1000];
+	FILE * fp = fopen("pshutdown.cfg", "r");
+	char ch;
+	while ((ch = fgetc(fp)) != EOF){
+		
+	}
+}
+
+char [] getRmCommand(){
+
+}
+
+
+void main(){
+	char []
+
+}
