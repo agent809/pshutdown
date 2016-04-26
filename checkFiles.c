81710f21e5cf71bd4ca81a2ce36b35a3804d426f
agent809 <mragent809@gmail.com>
2016-04-25 20:43:21 -0400

    Started on config file loader

--- checkFiles.c
+++ checkFiles.c
@@ -1,20 +1,64 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-char [] getInCommand(){
-	char file[1000];
-	FILE * fp = fopen("pshutdown.cfg", "r");
-	char ch;
-	while ((ch = fgetc(fp)) != EOF){
-		
+void readConfigFile();
+void analyze(char *inputCommand, int size);
+char installCommand[50];
+char uninstallCommand[50];
+
+
+void analyze(char *inputCommand, int size){
+	for (int i = 0; i < size; i++){
+		printf("%c", inputCommand[i]);
 	}
+	printf("\n");
 }
 
-char [] getRmCommand(){
 
+void readConfigFile(){
+	FILE * fp = fopen("pshutdown.cfg", "r");
+	//Gets the size of the file
+	fseek(fp, 0, SEEK_END);
+	int size = ftell(fp);
+	rewind(fp);
+
+	char ch;
+	bool copy = true;
+	char file[size];
+	int counter = 0;
+	for (int i = 0; i < size; i++){
+		ch = fgetc(fp);
+		if (ch == '{'){
+			copy = false;
+			continue;
+		}else if(ch == '}'){
+			copy = true;
+			continue;
+		}else if (ch == '\n'){
+			continue;
+		}else if(ch == ';'){
+			analyze(file, counter);
+			counter = 0;
+			continue;
+		}
+
+		if (copy){
+			file[counter] = ch;
+			counter += 1;
+		}
+	}
+	
+	fclose(fp);
 }
-
 
 void main(){
-	char []
-
+	/*
+	char * p;
+	p = getInCommand();
+	for (int i = 0; i <= 1000; i++){
+		printf("%c", *(p + i));
+	}
+	*/
+	readConfigFile();
 }
